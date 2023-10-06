283789e84fa3446377d6f84ad8be8756fd14b565
SaidAbedi <abedi.designs@gmail.com>
2023-10-06 15:23:56 -0500

    update packages

--- veryable_takehome/src/App.tsx
+++ veryable_takehome/src/App.tsx
@@ -2,17 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-// import { useQuery } from '@apollo/client';
-// import { GET_LOCATIONS } from './services/getLocations';
+import { Grid } from '@mui/material'
 
 
 function App() {
   const [count, setCount] = useState(0)
-  // const { data } = useQuery(GET_LOCATIONS);
 
 
   return (
     <>
+    <Grid>
+      here
+    </Grid>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
