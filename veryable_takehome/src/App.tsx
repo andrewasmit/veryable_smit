0d1f64236ea5368d7c8fb1029b3fa6892ca52e4e
SaidAbedi <abedi.designs@gmail.com>
2023-10-06 15:25:45 -0500

    remove unused code

--- veryable_takehome/src/App.tsx
+++ veryable_takehome/src/App.tsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Grid } from '@mui/material'
-
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,9 +9,6 @@ function App() {
 
   return (
     <>
-    <Grid>
-      here
-    </Grid>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
