6a586d1d472e31ef09656b97ad84b38fcffbc671
andrew <andrewasmit@hotmail.com>
2023-10-07 16:59:30 -0500

    feat: all data present dynamic and styled

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -1,9 +1,9 @@
 // External Dependencies
-import { Backdrop, Button, Card, Typography } from "@mui/material"
+import { useMemo } from "react";
+import { Backdrop, Button, Card, List, Typography } from "@mui/material"
 
 // Internal Dependencies
 import { Language, State } from "../../gql/getCountries";
-import { useMemo } from "react";
 
 
 interface PopupProps{
@@ -17,8 +17,6 @@ interface PopupProps{
   name: string;
 }
 
-// Languages, currency, number of states, flag, 
-
 function Popup({ 
   handleClose, 
   isOpen,
@@ -30,6 +28,28 @@ function Popup({
   name
 }: PopupProps) {
 
+  const cardStyles = { 
+    width: 700, 
+    maxHeight: 900,
+    padding:6, 
+    margin:'auto',
+    maxWidth: 800,
+    display: 'block',
+    textAlign: 'center'
+};
+
+  const listStyles = {
+    width: '100%',
+    maxWidth: 300,
+    margin: 'auto',
+    bgcolor: 'background.paper',
+    position: 'relative',
+    overflow: 'auto',
+    maxHeight: 250,
+    '& ul': { padding: 0.5 },
+  };
+
+
   const statesToDisplay = useMemo(()=>{
     if (states.length === 0){
       return <Typography variant="body1">-This country has 0 states.</Typography>
@@ -40,40 +60,52 @@ function Popup({
     })
   }, [states])
 
+
   const languagesToDisplay = useMemo(()=>{
     return languages.map(language=>{
       return <Typography variant="body1">- {language.native}</Typography>
     }) 
   }, [languages]);
 
-  const cardStles = { 
-    width: 700, 
-    padding:8, 
-    margin:'auto',
-    maxWidth: 800,
-    // display: 'inline-block',
-    textAlign: 'center'
-}
-
+  
   return (
     <div>
       <Backdrop
         sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
         open={isOpen}
       >
-        <Card elevation= {4} sx={cardStles}>
+        <Card elevation= {4} sx={cardStyles}>
           <Typography variant="h3" >{name}</Typography>
           <Typography variant="h2" >{emoji}</Typography>
           <Typography variant="h6" ><strong>Continent:</strong> {continent}</Typography>
           <Typography variant="h6" ><strong>Currency:</strong> {currency}</Typography>
 
           <Typography variant="h6" ><strong>Languages:</strong></Typography>
-          {languagesToDisplay}
+          <List
+            sx={listStyles}
+            subheader={<li />}
+          >
+            {languagesToDisplay}
+          </List>
 
-          <Typography variant="h6" ><strong>States:</strong></Typography>
-          {statesToDisplay}
+          <Typography variant="h6" >
+            <strong>States: {states.length > 0 ? `(${states.length})` : ''}</strong>
+          </Typography>
+          <List
+            sx={listStyles}
+            subheader={<li />}
+          >
+            {statesToDisplay}
+          </List>
 
-          <Button size="large" variant="contained" onClick={handleClose}>Close</Button>
+          <Button 
+            size="large" 
+            variant="contained" 
+            onClick={handleClose}
+            sx={{ marginTop: 6 }}
+          >
+            Close
+          </Button>
         </Card>
       </Backdrop>
     </div>
