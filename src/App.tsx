9cea089e4ac7f1d9952eea34c6ed3d220f273985
andrew <andrewasmit@hotmail.com>
2023-10-06 20:43:29 -0500

    feat: added background hero image

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
   }
   
   return (
-    <div id="main container">
+    <div id="main-container">
       <Typography variant="h1">Smit's Take Home Assignment</Typography>
       <Typography variant="h3">Veryable</Typography>
     </div>
