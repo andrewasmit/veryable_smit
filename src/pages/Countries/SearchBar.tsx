5b3f9d67975c1f979d41470b037a5b6e2a621f3f
andrew <andrewasmit@hotmail.com>
2023-10-07 20:35:33 -0500

    feat: SearchBar functioning and TS errors handled

--- src/pages/Countries/SearchBar.tsx
+++ src/pages/Countries/SearchBar.tsx
@@ -1,6 +1,12 @@
 // External Dependencies
-import { Dispatch, SetStateAction, useCallback, useState } from "react";
-import { Autocomplete, Button, TextField } from "@mui/material"
+import { 
+  Dispatch, 
+  SetStateAction, 
+  useCallback, 
+  useMemo, 
+  useState 
+} from "react";
+import { Button, TextField } from "@mui/material"
 
 // Internal Dependencies
 import { Country } from "../../gql/getCountries";
@@ -14,43 +20,34 @@ interface SearchBarProps{
 // Component Definition
 function SearchBar({ data, handleSearchFilter }: SearchBarProps) {
 
-  // const [search, setSearch] = useState('');
+  const [search, setSearch] = useState('');
 
-  // const options = [
-  //   {label: 'Yo Mamma'},
-  //   {label: 'Yo Daddy'},
-  //   {label: 'Yo Frieeeend'}
-  // ]
+  const allData = useMemo(()=>{
+    return data;
+  },[])
 
-  // const handleSearchText = useCallback((e)=>{
-  //   setSearch(e.target.value)
-  // }, [search]);
+  const handleSearchText = useCallback((e: React.ChangeEvent<HTMLInputElement>)=>{
+    setSearch(e.target.value)
+  }, [search]);
 
-  const handleFilterData = useCallback((e)=>{
-    const text = e.target.value;
-    const filteredData = data?.filter(country=>country.name.toLowerCase().includes(text.toLowerCase()))
+  const handleFilterData = useCallback(()=>{
+    const filteredData = allData?.filter(country=>country.name.toLowerCase().includes(search.toLowerCase()))
     handleSearchFilter(filteredData)
-  },[data]);
+  },[data, search]);
 
   const handleResetFilter = useCallback(()=>{
     handleSearchFilter(data)
+    setSearch('');
   },[]);
 
   return (
-    // <Autocomplete
-    //   disablePortal
-    //   id="searchbar"
-    //   options={options}
-    //   sx={{ width: 300 }}
-    //   renderInput={(params) => <TextField {...params} label="Search" />}
-    // />
     <>
       <TextField 
         label="Search for a Country" 
-        // value={search} 
-        onChange={handleFilterData}
+        value={search} 
+        onChange={handleSearchText}
       />
-      <Button onClick={handleFilterData} variant="contained" >Search</Button>
+      <Button type="submit" onClick={handleFilterData} variant="contained" >Search</Button>
       <Button onClick={handleResetFilter} variant="outlined" >Clear</Button>
     </>
   )
