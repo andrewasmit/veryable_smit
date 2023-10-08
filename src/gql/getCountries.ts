f17834258242ecaaa0f745e3b301f6c8061f2611
andrew <andrewasmit@hotmail.com>
2023-10-08 09:38:37 -0500

    chore: cleanup comments

--- src/gql/getCountries.ts
+++ src/gql/getCountries.ts
@@ -48,7 +48,6 @@ const GET_COUNTRIES = gql`
   }
 `;
 
-// export const useGetCountries = (): QueryResult<Country[]> => {
 export const useGetCountries = () => {
   return useQuery(GET_COUNTRIES);
 };
