3670102e0807c9b7e82cf7d16fa8c63da3b24651
andrew <andrewasmit@hotmail.com>
2023-10-09 09:19:22 -0500

    feat: main components of Question card up and running

--- src/pages/Game/QuestionCard.tsx
+++ src/pages/Game/QuestionCard.tsx
@@ -1,98 +1,83 @@
 // External Dependencies
-import { useCallback, useState } from 'react'
+import { useState } from 'react'
 import { 
   Button, 
   Typography, 
   Card, 
   CardActions, 
-  Box,
-  Fab
+  Box
 } from '@mui/material'
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 // Internal Dependencies
 import { Country } from '../../gql/getCountries'
-import Popup from '../../components/Popup/Popup';
 import { useIsOpen } from '../../utils/useIsOpen';
-import { useAppDispatch, useAppSelector } from '../../redux/hooks';
-import { addToFavorites, removeFromFavorites } from '../../redux/favoriteSlice';
+
 
 // Component Definition
-function CountryCard({
-  continent,
-  currency,
-  emoji,
-  languages,
-  name,
-  states,
-}: Country ) {
+function CountryCard({ data }: Country ) {
 
-  const { countries: favoriteCountries } = useAppSelector(state=>state.favorites)
-  const dispatch = useAppDispatch();
-
+  const [isAnswered, setIsAnswered] = useState(true);
   const { isOpen, handleOpen, handleClose } = useIsOpen();
-  const [isFavorite, setIsFavorite] = useState(favoriteCountries.some(country=>country.name === name));
 
-  const handleClickFavorite = useCallback(()=>{
-    if (!isFavorite){
-      const newFavorite: Country = {
-        continent,
-        currency,
-        emoji,
-        languages,
-        name,
-        states
-      }
-      dispatch(addToFavorites(newFavorite))
-    } else {
-      dispatch(removeFromFavorites(name))
-    }
-    setIsFavorite(!isFavorite);
-  }, [isFavorite]);
+  const btnStyles = {
+    marginTop: 2,  
+    ':hover':{ 
+      color: '#333366', 
+      backgroundColor: '#ffff33', 
+      boxShadow: 15
+    },
+    ':not(:first-of-type)':{
+      marginLeft: 0,
+    },
+    padding: 1,
+    fontSize: 20
+  }
 
   return (
     <Box component="div" sx={{ padding: 2 }}>
-      <Card elevation= {3} sx={{ textAlign: 'center', maxWidth: 450, padding:4, margin:'auto', ':hover':{ boxShadow:20 } }}>
-        <Typography variant='h1'>{emoji}</Typography>
-        <Typography variant='h5'>{name}</Typography>
-        <Typography variant='h6'>{continent.name}</Typography>
-        <CardActions sx={{ display: 'flex', justifyContent:'center', alignContent: 'space-between' }}>
+      <Card elevation= {3} sx={{ textAlign: 'center', maxWidth: 550, padding:4, margin:'auto', ':hover':{ boxShadow:20 } }}>
+        <Typography variant='h3'>Guess the flag!</Typography>
+        <Typography variant='h1'>{data.emoji}</Typography>
+        
+        {isAnswered &&
+          <Typography variant='h5'><strong>Country:</strong> {data.name}</Typography>
+        }
+
+        <CardActions sx={{ display: 'block', margin:'auto' }}>
           <Button 
             size="small" 
             onClick={handleOpen}
             variant='contained'
             color='primary'
-            sx={{ marginRight: 3 }}
+            fullWidth
+            sx={btnStyles}
           >
-            Show Details
+            Option 1
           </Button>
 
-          <Fab 
-            onClick={handleClickFavorite} 
-            size='small'
-            color= {isFavorite ? 'error' : 'info'}
-            aria-label="favorite"
-            sx={{ padding: 2 }}
+          <Button 
+            size="small" 
+            onClick={handleOpen}
+            variant='contained'
+            color='primary'
+            fullWidth
+            sx={btnStyles}
           >
-            {isFavorite ? 
-              <FavoriteIcon /> :
-              <FavoriteBorderIcon />
-            }
-          </Fab>
+            Option 2
+          </Button>
+
+          <Button 
+            size="small" 
+            onClick={handleOpen}
+            variant='contained'
+            color='primary'
+            fullWidth
+            sx={btnStyles}
+          >
+            Option 3
+          </Button>
+
         </CardActions>
-
-        <Popup 
-          isOpen={isOpen} 
-          handleClose={handleClose}
-          currency={currency}
-          continent={continent.name}
-          languages={languages}
-          name={name}
-          states={states}
-          emoji={emoji}
-        />
-
       </Card>
     </Box>
   )
--- src/pages/Game/index.tsx
+++ src/pages/Game/index.tsx
@@ -2,25 +2,28 @@
 import { useMemo } from "react"
 import { Typography } from "@mui/material"
 
-
 // Internal Dependencies
 import { CountriesProps } from "../Countries"
 
-
 // Local Dependencies
+import QuestionCard from './QuestionCard'
 
 
 function Game({ data }:CountriesProps) {
 
-  const randomIdx = Math.floor(Math.random() * data.countries?.length)
-  console.log("RANDOM: ", randomIdx);
+  const randomIdx = Math.floor(Math.random() * data.countries?.length);
+  const randomCountry = data.countries[randomIdx];
+  console.log("RANDOM: ", randomCountry);
 
-  const questionToDisplay = data.countries[randomIdx]
+  const questionToDisplay = useMemo(()=>{
+    return <QuestionCard data={randomCountry} />
+  },[randomCountry])
 
   
   return (
     <>
-      <Typography variant="h1">This is the Game Page</Typography>
+      <Typography variant="h2" sx={{ textAlign:'center',color: '#fff' }} >This is the Game Page</Typography>
+      { questionToDisplay }
     </>
   )
 }
